99964d2cde25497a55dcb02be003d36f64e798ab
AshishDixit0 <ashish.dixit@dizartsolutions.com>
2024-07-16 14:06:24 +0530

    custom button

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,21 @@
+import Button from '@/components/Button/Button';
 import {View, Text, StyleSheet, Platform } from 'react-native';
 
-
+const fun=()=>{
+  return(
+    console.log("hello")
+  )
+}
 
 export default function HomeScreen() {
   return (
 <View style={styles.container}>
  <Text>hello wolrd</Text> 
+ <Button 
+ title='hello'
+ onPress={fun}
+ containerStyles={{width:300}}
+ />
 </View>
   );
 }
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,24 +1,37 @@
-import { View, Text } from 'react-native'
+import {Text,StyleSheet,TouchableOpacity  } from 'react-native'
 import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+
 
 interface ButtonProps{
     onPress:()=>void;
     title?: string;
-    textStyles?: string;
-    containerStyles?: string;
+    textStyles?: {};
+    containerStyles?: {};
 
 }
 
-const Button = ({onPress, title, textStyles = "", containerStyles=""}: ButtonProps) => {
+const Button = ({onPress, title, textStyles = {}, containerStyles={}}: ButtonProps) => {
   return (
+   
     <TouchableOpacity activeOpacity={0.7}
-     style={{backgroundColor:'#ef9d1f', borderRadius:20, justifyContent:'center'}}
+     style={{ ...styles.containerStyles, ...containerStyles }}
     onPress={onPress}
     >
-      <Text style={{fontSize:600}}>{title}</Text>
+      <Text style={{ fontSize: 16, ...textStyles }}>{title}</Text>
     </TouchableOpacity>
+
   )
 }
 
 export default Button
+const styles = StyleSheet.create({
+  containerStyles:{
+    height: 50,
+    width:10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    backgroundColor:"#ef9d1f",
+  
+  },
+});
