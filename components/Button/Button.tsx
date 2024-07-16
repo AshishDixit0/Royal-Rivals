6358fd998cd94d2b099c7da921cf0b3be0307f5e
zemanisterD <vimanyu.sharma@dizartsolutions.com>
2024-07-16 17:08:16 +0530

    button fixes color

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,11 +1,13 @@
 import { Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+import { Colors } from "@/constants/Colors";
 
 interface ButtonProps {
   onPress: () => void;
   title?: string;
   textStyles?: {};
   containerStyles?: {};
+  bgColor?: string;
 }
 
 const Button = ({
@@ -13,11 +15,12 @@ const Button = ({
   title,
   textStyles = {},
   containerStyles = {},
+  bgColor = Colors.golden
 }: ButtonProps) => {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
-      style={{ ...styles.containerStyles, ...containerStyles }}
+      style={{ ...styles.containerStyles, ...containerStyles, backgroundColor: bgColor }}
       onPress={onPress}
     >
       <Text style={{ fontSize: 16, ...textStyles }}>{title}</Text>
@@ -33,6 +36,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 30,
-    backgroundColor: "golden",
   },
 });
