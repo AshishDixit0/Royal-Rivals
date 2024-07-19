b0b707f511bccda8ffcaa29634f1b8f6e549cda0
AshishDixit0 <ashish.dixit@dizartsolutions.com>
2024-07-19 16:27:33 +0530

    color conflict

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,27 +7,32 @@ const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-  BACKGROUND_COLOR: '#160120', 
-  golden: '#ef9c1f',
-  whiteButton: '#fefffe',
-    header_Color:"#3C0A45",
-  home_bg:"#4e1e49"
-  header_Color:"#3C0A45",
+
+light: {
+  text: '#11181C',
+  background: '#fff',
+  tint: tintColorLight,
+  icon: '#687076',
+  tabIconDefault: '#687076',
+  tabIconSelected: tintColorLight,
+},
+dark: {
+  text: '#ECEDEE',
+  background: '#151718',
+  tint: tintColorDark,
+  icon: '#9BA1A6',
+  tabIconDefault: '#9BA1A6',
+  tabIconSelected: tintColorDark,
+},
+BACKGROUND_COLOR: '#160120', 
+golden: '#ef9c1f',
+whiteButton: '#fefffe',
+purpleNavigation: '#3c0b44',
+accountsPurplePage: '#160021',
+cardBackground: '#3c0b44',
+cardHolderbackground: '#160021',
+accountBackground: '#4e1e49',
+checkboxBorder: '#EF9D1F',
+   header_Color:"#3C0A45",
   home_bg:"#4e1e49"
 };
